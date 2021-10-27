1d030a9001da6eb8df496c0729c17b944dc09520
Ronny <ronny761@hityash.org>
2021-10-27 11:22:09 +0300

    stam

---
+++ src/Stam.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Stam {
+    public static void main(String[] args) {
+        //Scanner s = new Scanner(System.in);
+        int n1 = 10;
+        int n2 = 4;
+
+        double result = (double) n1 / n2;
+        System.out.println(result);
+    }
+}
